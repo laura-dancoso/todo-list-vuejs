c4d2917ac9880f9bcbc3de8f359332e4b37ea613
Laura-Dancoso <lauradancoso@gmail.com>
2021-01-11 17:58:43 -0300

    todo list uploaded

---
+++ vueJs.js
@@ -0,0 +1,31 @@
+const app = new Vue({
+    el:'#app',
+    data:{
+        titulo:'TODO LIST',
+        items:[],
+        newItem:'',
+        id:0
+    },
+    methods:{
+        addItem(){
+            if(this.newItem != ''){
+                this.items.push({
+                    msg: this.newItem, checked:false, id: this.id
+                });
+                this.id++;
+                this.newItem='';
+                this.newItem.focus();
+            }
+        },
+        deleteItem(id){
+           // see https://gist.github.com/scottopolis/6e35cf0d53bae81e6161662e6374da04
+           //encuentro el id del elemento a borrar
+           let removeIndex = this.items.map(i=>i.id).indexOf(id);
+           this.items.splice(removeIndex,1);
+           this.newItem.focus();
+        },
+        changeIcon(item){
+            return (item.checked) ? 'check_box' : 'check_box_outline_blank';
+         }
+    }
+})
